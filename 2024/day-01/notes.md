53d69dc4f45d42bff7addadb13c3d3a4cd519b9d
Silas Bartha <silas@exvacuum.dev>
2024-12-01 15:46:45 -0500

    Day 1, Part 1

---
+++ 2024/day-01/notes.md
@@ -0,0 +1,11 @@
+# Advent of Code 2024 Notes
+
+## Provided Information
+
+- Input is 2 lists of location IDs
+
+## Instructions
+
+- Pair up smallest number in left list with smallest number in right list and so on
+- Figure out the difference between each pair
+- Add up all the differences
--- 2024/day-01/src/main.rs
+++ 2024/day-01/src/main.rs
@@ -1,3 +1,34 @@
+use std::{fs::File, io::{self, BufRead}, path::PathBuf};
+
+use clap::Parser;
+use scan_rules::scan;
+use scan_rules::scan_rules_impl;
+
+#[derive(Parser)]
+struct Args {
+    pub input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let input_file = File::open(args.input).unwrap();
+    let mut list_a_sorted = vec![];
+    let mut list_b_sorted = vec![];
+    for line in io::BufReader::new(input_file).lines().flatten() {
+        scan! {
+            &line;
+            (let a: isize, "   ", let b: isize) => {
+                  list_a_sorted.push(a);    
+                  list_b_sorted.push(b);    
+            },
+        }.unwrap();
+    }
+    list_a_sorted.sort();
+    list_b_sorted.sort();
+
+    let mut sum = 0;
+    for (a, b) in list_a_sorted.into_iter().zip(list_b_sorted.into_iter()) {
+        sum += a.abs_diff(b) as usize;
+    }
+    println!("{sum}");
 }
